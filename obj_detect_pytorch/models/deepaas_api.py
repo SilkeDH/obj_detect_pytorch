b58f809dd07424cd27fcb068b6cd71c4b0db5ce8
root <silkedonayre@gmail.com>
2019-11-29 09:10:15 +0000

    Defining training code.

--- obj_detect_pytorch/models/deepaas_api.py
+++ obj_detect_pytorch/models/deepaas_api.py
@@ -12,10 +12,12 @@ from PIL import Image
 import obj_detect_pytorch.models.model_utils as mutils
 import obj_detect_pytorch.models.model_metrics as mmetrics
 import obj_detect_pytorch.models.create_resfiles as resfiles 
+import obj_detect_pytorch.dataset.make_dataset as mdata
 from fpdf import FPDF
 import cv2
 import ignite
 from torchvision import transforms, utils
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 
 
@@ -65,7 +67,85 @@ def get_train_args():
         ),
     }
 
+def get_model_instance_segmentation(num_classes):
+    # load an instance segmentation model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
+
+def get_transform(train):
+    t = []
+    t.append(transforms.ToTensor())
+    if train:
+        t.append(transforms.RandomHorizontalFlip(0.5))
+    return transforms.Compose(t)
+
+
 def train(train_args):
+    num_classes = 2 
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # use our dataset and defined transformations
+    dataset = mdata.PennFudanDataset('PennFudanPed', get_transform(train=True))
+    dataset_test = mdata.PennFudanDataset('PennFudanPed', get_transform(train=False))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+    
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
+    
+    
     #Training of the model:
     run_results = { "status": "Not implemented in the model (train)",
                     "train_args": [],
